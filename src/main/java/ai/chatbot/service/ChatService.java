708e13722b96ccc7608d251acdc5e15c48316ca0
danilo <d.cividin@hotmail.it>
2025-07-17 14:31:13 +0200

    code refactoring
    now we will more easily introduce more ChatService implementations.

--- src/main/java/ai/chatbot/service/ChatService.java
+++ src/main/java/ai/chatbot/service/ChatService.java
@@ -1,53 +1,8 @@
 package ai.chatbot.service;
 
-import ai.chatbot.client.OllamaClient;
-import ai.chatbot.dto.*;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.BadRequestException;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jboss.logging.Logger;
+import ai.chatbot.dto.ChatRequest;
+import ai.chatbot.dto.ChatResponse;
 
-import java.util.List;
-
-@ApplicationScoped
-public class ChatService {
-    private static final Logger LOG = Logger.getLogger(ChatService.class);
-
-    private final OllamaClient ollamaClient;
-
-    @ConfigProperty(name = "ollama.model", defaultValue = "llama3")
-    String model;
-
-    public ChatService(@RestClient OllamaClient ollamaClient) {
-        this.ollamaClient = ollamaClient;
-    }
-
-    public ChatResponse chat(ChatRequest request) {
-        LOG.debugf("Generating response using model: %s", model);
-        LOG.debugf("Prompt: %s", request.prompt());
-
-        List<String> installedModels = ollamaClient.getAvailableModels().models().stream()
-                .map(OllamaModelTag::name)
-                .toList();
-
-        LOG.debugf("Model Names: %s", installedModels);
-
-        boolean found = installedModels.stream()
-                .anyMatch(t -> t.trim().equalsIgnoreCase(model) || t.trim().split(":")[0].equalsIgnoreCase(model));
-
-        if (!found) {
-            throw new BadRequestException(
-                    "Modello '" + model + "' non disponibile. Modelli installati: " +
-                            String.join(", ", installedModels)
-            );
-        }
-
-        OllamaRequest ollamaRequest = new OllamaRequest(model, request.prompt(), false);
-        OllamaResponse ollamaResponse = ollamaClient.generate(ollamaRequest);
-
-        LOG.debugf("Ollama response: %s", ollamaResponse.response());
-
-        return new ChatResponse(ollamaResponse.response());
-    }
+public interface ChatService {
+    ChatResponse chat(ChatRequest request);
 }
--- src/main/java/ai/chatbot/web/GlobalExceptionMapper.java
+++ src/main/java/ai/chatbot/web/GlobalExceptionMapper.java
@@ -1,5 +1,6 @@
 package ai.chatbot.web;
 
+import ai.chatbot.exception.ConfigurationException;
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -13,12 +14,13 @@ public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
         return resolveError(e);
     }
 
-    private Response resolveError(Exception e) {
-        if(e instanceof BadRequestException) {
-            return buildResponse(Response.Status.BAD_REQUEST, e.getMessage());
-        } else {
-            return buildResponse(Response.Status.INTERNAL_SERVER_ERROR, "Internal error: " + e.getMessage());
-        }
+    private Response resolveError(Exception ex) {
+        // ConfigurationException
+        return switch(ex) {
+            case BadRequestException e -> buildResponse(Response.Status.BAD_REQUEST, e.getMessage());
+            case ConfigurationException e -> buildResponse(Response.Status.INTERNAL_SERVER_ERROR, "Errore di configurazione: " + e.getMessage());
+            default -> buildResponse(Response.Status.INTERNAL_SERVER_ERROR, "Internal error: " + ex.getMessage());
+        };
     }
 
     private Response buildResponse(Response.Status status, String error) {
